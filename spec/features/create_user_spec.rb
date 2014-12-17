93c24d5cd9cf94d780512c5fc27ed2d7e164dc4d
hpjaj <hmlevine@gmail.com>
2014-12-17 10:01:11 -0700

    Created RESTful routes for Users. Created form partial for a new user. Updated users_controller new method to get passing test for entering data into form.

--- spec/features/create_user_spec.rb
+++ spec/features/create_user_spec.rb
@@ -5,6 +5,8 @@ describe "A new user" do
   it "has a first and last name, email address and SSN" do
     visit "/"
     click_link "Create User"
+    expect( current_path ).to eq new_user_path 
+    fill_in 'First name', with: "John"
     
   end
 
