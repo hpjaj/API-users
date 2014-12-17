09cd18d3d00cfa2b9061c087c5e1d9a7f83def7c
hpjaj <hmlevine@gmail.com>
2014-12-17 10:06:54 -0700

    Updated users_controller with create method, accounting for both JSON and HTML formatted responses.  Added error message handling in application.html.erb file.  Passing test.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  respond_to :html, :json
+
   def index
   end
 
@@ -6,6 +9,31 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      respond_to do |format|
+        format.json { render json: @user.to_json, status: :created }
+        format.html { redirect_to @user }
+      end
+    else
+      respond_to do |format|
+        format.json { render json: @user.errors.full_messages }
+        format.html do
+          flash[:error] = @user.errors.full_messages
+          render :new
+        end
+      end
+    end
+  end
+
   def show
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :social_security_number)
+  end
+  
 end
--- spec/features/create_user_spec.rb
+++ spec/features/create_user_spec.rb
@@ -7,6 +7,10 @@ describe "A new user" do
     click_link "Create User"
     expect( current_path ).to eq new_user_path 
     fill_in 'First name', with: "John"
+    fill_in 'Last name', with: "Doe"
+    fill_in 'Email address', with: "John@example.com"
+    fill_in 'Social security number', with: "123456789"
+    click_button "Save"
     
   end
 
